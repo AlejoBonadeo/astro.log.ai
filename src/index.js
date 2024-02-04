2640537bff56ef463b23c621455d36ede4bc8596
Alejo Bonadeo <86572168+AlejoBonadeo@users.noreply.github.com>
2024-02-04 16:23:44 -0300

    Bug-fixes 04/02/2024

--- src/index.js
+++ src/index.js
@@ -3,142 +3,252 @@ import * as chart from "./chart";
 import * as text from "./text";
 import * as monitoring from "./monitoring";
 import * as configurator from "./configurator";
-import { applyTheme, displayErrorPage, displayLoader, getCurrentTime, isTransit, withErrorHandling } from "./utils";
+import {
+  applyTheme,
+  displayErrorPage,
+  displayLoader,
+  getCurrentTime,
+  isTransit,
+  withErrorHandling,
+} from "./utils";
 
 import "./styles/base.css";
 import "./styles/spinner.css";
 import "./styles/chart.css";
 import "./styles/settings.css";
 
-
 const UPDATE_INTERVAL = 1000;
 let chartUpdater;
 
-import { WebMidi } from 'webmidi';
+import { WebMidi } from "webmidi";
 
-WebMidi
-  .enable()
+WebMidi.enable()
   .then(() => console.log("WebMidi enabled!"))
-  .catch(err => alert(err));
+  .catch((err) => alert(err));
 
-let bodies = ["sun", "moon", "mercury", "venus", "mars", "jupiter", "saturn", "uranus", "neptune", "pluto"];
-let signs = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"];
-let aspects = ["conjunction","opposition", "trine", "square", "sextile"];
+let bodies = [
+  "sun",
+  "moon",
+  "mercury",
+  "venus",
+  "mars",
+  "jupiter",
+  "saturn",
+  "uranus",
+  "neptune",
+  "pluto",
+];
+let signs = [
+  "aries",
+  "taurus",
+  "gemini",
+  "cancer",
+  "leo",
+  "virgo",
+  "libra",
+  "scorpio",
+  "sagittarius",
+  "capricorn",
+  "aquarius",
+  "pisces",
+];
+let aspects = ["conjunction", "opposition", "trine", "square", "sextile"];
+
+/**
+ * Calculate polar coordinates theta for a celestial body relative to a Cartesian origin.
+ *
+ * @param {string} celestial_body - The name of the celestial body.
+ * @param {Object} cartesian_00 - The Cartesian coordinates of the new origin point.
+ * @param {number} cartesian_00.x - The x-coordinate of the origin point.
+ * @param {number} cartesian_00.y - The y-coordinate of the origin point.
+ * @returns {number} - The polar coordinates theta in degrees.
+ */
+function calculateTheta(celestial_body, cartesian_00) {
+  const { x, y } = document
+    .getElementById(
+      `chart-astrology-radix-planets-${celestial_body[0].toUpperCase()}${celestial_body.slice(
+        1
+      )}`
+    )
+    ?.getBoundingClientRect();
+  const coords = {
+    x: x - cartesian_00.x,
+    y: cartesian_00.y - y,
+  };
+
+  let theta = Math.atan(coords.y / coords.x);
+  if (coords.x < 0 || coords.y < 0) theta += Math.PI;
+  if (coords.x > 0 && coords.y < 0) theta += Math.PI;
+  // Rad to deg
+  theta *= 180 / Math.PI;
+  // Midi input does not take 0 or flloats
+  theta = Math.round(theta) || 1;
+
+  return theta;
+}
 
 function run({ origin, transit, settings }) {
-    console.info("[Configuration] %o", { origin, transit, settings });
+  console.info("[Configuration] %o", { origin, transit, settings });
 
-    displayLoader(true);
-    if (!origin || !settings) return;
+  displayLoader(true);
+  if (!origin || !settings) return;
 
-    if (chartUpdater) {
-        clearInterval(chartUpdater);
-        chartUpdater = null;
-    }
+  if (chartUpdater) {
+    clearInterval(chartUpdater);
+    chartUpdater = null;
+  }
 
-    let dataRadix = calculate(origin, settings);
+  let dataRadix = calculate(origin, settings);
 
-    let dataTransit;
+  let dataTransit;
+  if (isTransit(settings)) {
+    dataTransit = calculate(transit, settings);
+  }
+
+  text.display(dataRadix, dataTransit, origin, transit, settings);
+  chart.draw(dataRadix, dataTransit, settings);
+
+  applyTheme(settings.stroke, settings.bg);
+  displayLoader(false);
+
+  const updateChart = withErrorHandling((currentTime) => {
     if (isTransit(settings)) {
-        dataTransit = calculate(transit, settings);
-    }
+      dataTransit = calculate({ ...transit, ...currentTime }, settings);
+    } else {
+      dataRadix = calculate({ ...origin, ...currentTime }, settings);
+      console.log(dataRadix);
 
-    text.display(dataRadix, dataTransit, origin, transit, settings);
-    chart.draw(dataRadix, dataTransit, settings);
+      const cartesian_00 = {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2,
+      };
 
-    applyTheme(settings.stroke, settings.bg);
-    displayLoader(false);
+      // for each output on WebMidi
+      for (let p = 0; p < WebMidi.outputs.length; p++) {
+        var output = WebMidi.outputs[p];
+        console.log("channel", channel);
 
-    const updateChart = withErrorHandling((currentTime) => {
-        if (isTransit(settings)) {
-            dataTransit = calculate({ ...transit, ...currentTime }, settings);
-        } else {
-            dataRadix = calculate({ ...origin, ...currentTime }, settings); 
-            console.log(dataRadix);
-            
-            // for each output on WebMidi
-            for (let p = 0; p < WebMidi.outputs.length; p++) {
-                var output = WebMidi.outputs[p];
-                // console.log("channel", channel);
-                
-                // iterate throiugh all bodies
-                for (let i = 0; i < bodies.length; i++) {
-                    var channel = output.channels[i + 1];
-                    var key = bodies[i];
-                    var body = dataRadix.horoscope.Ephemeris[bodies[i]];
-                    var alt = Math.floor(body.position.altaz.topocentric.altitude);
-                    var az = Math.floor(body.position.altaz.topocentric.azimuth);
-                    var house = 13;
-                    if (dataRadix.horoscope._celestialBodies[bodies[i]].House)
-                        house = signs.indexOf(dataRadix.horoscope._celestialBodies[bodies[i]].House.key);
+        // iterate throiugh all bodies
+        for (let i = 0; i < bodies.length; i++) {
+          var channel = output.channels[i + 1];
+          var key = bodies[i];
+          var body = dataRadix.horoscope.Ephemeris[bodies[i]];
+          console.log(body);
+          var alt = Math.floor(body.position.altaz.topocentric.altitude);
+          var house = 13;
+          if (dataRadix.horoscope._celestialBodies[bodies[i]].House)
+            house = signs.indexOf(
+              dataRadix.horoscope._celestialBodies[bodies[i]].House.key
+            );
 
-                    var sign = 13;
-                    if (dataRadix.horoscope._celestialBodies[bodies[i]].Sign)
-                        sign = signs.indexOf(dataRadix.horoscope._celestialBodies[bodies[i]].Sign.key);
+          var sign = 13;
+          if (dataRadix.horoscope._celestialBodies[bodies[i]].Sign)
+            sign = signs.indexOf(
+              dataRadix.horoscope._celestialBodies[bodies[i]].Sign.key
+            );
 
-                    console.log(body.key, alt, az, house, sign);             
-                    
-                    // AZM 
-                    var az_channel = Math.floor(az / 128) + 1
-                    var az_remainder = az % 128;
+          const theta = calculateTheta(key, cartesian_00);
 
-                    for (let j = 1; j < az_channel; j++) 
-                        channel.sendControlChange(j, 127);
-                    channel.sendControlChange(az_channel, az_remainder);
-                    for (let j = az_channel + 1; j < 4; j++) 
-                        channel.sendControlChange(j, 0);
+          console.log(body.key, alt, theta, house, sign);
 
-                    // ALT
-                    if (alt < 0) {
-                        channel.sendControlChange(4, Math.abs(alt) );
-                        channel.sendControlChange(5, 0);
-                    }
-                    else {
-                        channel.sendControlChange(4, 0);
-                        channel.sendControlChange(5, alt);
-                    }
+          // AZM
+          const theta_channel = Math.floor(theta / 128);
+          const theta_remainder = theta % 128;
 
-                    // HOUSE
-                    channel.sendControlChange(6, house);
+          for (let j = 1; j < theta_channel; j++)
+            channel.sendControlChange(j, 127);
+          channel.sendControlChange(theta_channel, theta_remainder);
+          for (let j = theta_channel + 1; j < 4; j++)
+            channel.sendControlChange(j, 0);
 
-                    // SIGN
-                    channel.sendControlChange(7, sign);
+          // ALT
+          if (alt < 0) {
+            channel.sendControlChange(4, Math.abs(alt));
+            channel.sendControlChange(5, 0);
+          } else {
+            channel.sendControlChange(4, 0);
+            channel.sendControlChange(5, alt);
+          }
 
-                    // ASPECTS
-                    if (dataRadix.horoscope._aspects.points[key] != undefined) {
-                        var aspects_list = dataRadix.horoscope._aspects.points[key];
-                        console.log(aspects_list);
-    
-                        if (dataRadix.horoscope._aspects.points[key] == undefined) 
-                            continue;
-    
-                        for (let j = 0; j < aspects_list.length; j++) {
-                            var o = aspects_list[j].point1Key;
-                            if (o == key) 
-                                p = aspects_list[j].point2Key;
-                            var t = aspects_list[j].aspectKey;
-                            if (bodies.includes(p) && aspects.includes(t)) {
-                                console.log("aspect:", key, "with", p, t);
-                                channel.sendControlChange(8 + bodies.indexOf(p), aspects.indexOf(t));
-                            }
-                        }
-                    }
-                }
+          // HOUSE
+          channel.sendControlChange(6, house);
+
+          // SIGN
+          channel.sendControlChange(7, sign);
+
+          // ASPECTS
+          if (dataRadix.horoscope._aspects.points[key] != undefined) {
+            var aspects_list = dataRadix.horoscope._aspects.points[key];
+            console.log(aspects_list);
+
+            if (dataRadix.horoscope._aspects.points[key] == undefined) continue;
+
+            const valid_aspects_planets = [];
+
+            for (let j = 0; j < aspects_list.length; j++) {
+              var o = aspects_list[j].point1Key;
+              if (o == key) {
+                p = aspects_list[j].point2Key;
+              } else {
+                p = o;
+              }
+              valid_aspects_planets.push(p);
+              var t = aspects_list[j].aspectKey;
+              if (bodies.includes(p) && aspects.includes(t)) {
+                console.log("aspect:", key, "with", p, t);
+                channel.sendControlChange(
+                  8 + bodies.indexOf(p),
+                  aspects.indexOf(t)
+                );
+              }
             }
-        }
-        text.displayTime(dataRadix, dataTransit, settings);
-        chart.draw(dataRadix, dataTransit, settings);
-    }, displayErrorPage);
 
-    if (origin.isCurrentTime || (isTransit(settings) && transit.isCurrentTime)) {
-        chartUpdater = setInterval(() => updateChart(getCurrentTime(), settings), UPDATE_INTERVAL);
+            const no_aspects = bodies.filter(
+              (body) => !valid_aspects_planets.includes(body) && body !== key
+            );
+
+            for (const no_aspect of no_aspects) {
+              console.log("no aspect:", key, "with:", no_aspect);
+            }
+          }
+        }
+      }
     }
+    text.displayTime(dataRadix, dataTransit, settings);
+    chart.draw(dataRadix, dataTransit, settings);
+  }, displayErrorPage);
+
+  if (origin.isCurrentTime || (isTransit(settings) && transit.isCurrentTime)) {
+    const currentTime = getCurrentTime();
+    updateChart(currentTime, settings);
+    chartUpdater = setInterval(
+      () => updateChart(currentTime, settings),
+      UPDATE_INTERVAL
+    );
+  } else {
+    const time = new Date(
+      origin.year,
+      origin.month - 1,
+      origin.date,
+      origin.hour,
+      origin.minute
+    );
+    updateChart(time, settings);
+    chartUpdater = setInterval(
+      () => updateChart(time, settings),
+      UPDATE_INTERVAL
+    );
+  }
 }
 
 withErrorHandling(async () => {
-    monitoring.init();
-    text.init();
-    chart.init();
-    configurator.init(run);
-    run(await configurator.getParameters(new URLSearchParams(window.location.search)));
+  monitoring.init();
+  text.init();
+  chart.init();
+  configurator.init(run);
+  run(
+    await configurator.getParameters(
+      new URLSearchParams(window.location.search)
+    )
+  );
 }, displayErrorPage)();
